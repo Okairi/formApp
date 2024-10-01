9a5f6f558067e2f7d6405bc8b9304ae474ff31cd
Alessandro <anicho@ammper.com>
2024-10-01 16:04:36 -0500

    add menus and components reactive forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () =>
+      import('./reactive/reactive.module').then((x) => x.ReactiveModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((x) => x.AuthModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
